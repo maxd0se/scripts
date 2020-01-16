5aad35f6b84178e4e7e887c27065e7bb3b51d6c3
maxd0se <49950443+maxd0se@users.noreply.github.com>
2020-01-16 10:24:06 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,17 @@ echo 'Upgrading packages...'
 sudo apt full-upgrade -y
 
 echo 'Installing packages...'
-sudo apt install apt-transport-https ca-certificates curl software-properties-common git build-essential emacs -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common build-essential emacs -y
 
 echo 'Installing Docker...'
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt install docker-ce docker-compose
 sudo systemctl enable docker
+
+echo 'Installing docker-elk...'
+git clone https://github.com/deviantony/docker-elk.git
+cd docker-elk
+sudo docker-compose up -d
 
 echo 'Finished!'
