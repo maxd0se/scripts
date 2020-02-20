fe5d5f8c959c982fdde4fdb1bbc3b5163323ef35
maxd0se <49950443+maxd0se@users.noreply.github.com>
2020-02-20 14:03:07 -0600

    Add files via upload

---
+++ init.el
@@ -0,0 +1,58 @@
+;; .emacs.d/init.el
+;; ===================================
+;; MELPA Package Support
+;; ===================================
+;; Enables basic packaging support
+(require 'package)
+;; Adds the Melpa archive to the list of available repositories
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+;; Initializes the package infrastructure
+(package-initialize)
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+  ;; Installs packages
+;;
+;; myPackages contains a list of package names
+(defvar myPackages
+  '(cyberpunk-theme
+    elpy
+    magit
+    pyenv-mode
+    pyfmt
+    pygen
+    python-info
+    python-mode
+    python-x
+    treemacs
+    )
+  )
+;; Scans the list in myPackages
+;; If the package listed is not already installed, install it
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      myPackages)
+;; ===================================
+;; Basic Customization
+;; ===================================
+(setq inhibit-startup-message t)    ;; Hide the startup message
+(load-theme 'cyberpunk t)            ;; Load cyberpunk theme
+(elpy-enable)
+(add-hook 'emacs-startup-hook 'treemacs)
+;; User-Defined init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit pyenv-mode pyfmt pygen python-info python-mode python-x treemacs cyberpunk-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
