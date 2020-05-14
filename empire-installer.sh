130d6b840de1492a267f0f28cfc299dc457d6748
maxd0se <49950443+maxd0se@users.noreply.github.com>
2020-05-14 11:59:06 -0500

    Update empire-installer.sh
    fixed flags

--- empire-installer.sh
+++ empire-installer.sh
@@ -9,8 +9,8 @@ sudo apt install git python3-pip build-essential software-properties-common -y
 echo "installing Empire"
 git clone https://github.com/BC-SECURITY/Empire.git
 cd Empire
-sudo ./setup/install.sh
+echo | sudo ./setup/install.sh
 echo "starting Empire"
-sudo ./empire --rest
+sudo ./empire -rest
 dig +short myip.opendns.com @resolver1.opendns.com
 echo "empire is running on port 1337"
