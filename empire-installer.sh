261b2d37f42e072871be69384e1896674d08e928
maxd0se <49950443+maxd0se@users.noreply.github.com>
2020-05-14 11:24:05 -0500

    Add files via upload
    bash installer for powershell empire

---
+++ empire-installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "updating packages"
+sudo apt update
+echo "upgrading packages"
+sudo apt full-upgrade -y
+echo "installing prerequisite packages"
+sudo apt install git python3-pip build-essential software-properties-common -y
+echo "installing Empire"
+git clone https://github.com/BC-SECURITY/Empire.git
+cd Empire
+sudo ./setup/install.sh
+echo "starting Empire"
+sudo ./empire --rest
+dig +short myip.opendns.com @resolver1.opendns.com
+echo "empire is running on port 1337"
